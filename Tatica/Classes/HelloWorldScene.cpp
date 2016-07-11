67f44c9c0088ac25bb85f342b4250d2efdb9c89c
aquastyle <tjsgh0728@naver.com>
2016-07-11 15:02:38 +0900

    map 적용

--- Tatica/Classes/HelloWorldScene.cpp
+++ Tatica/Classes/HelloWorldScene.cpp
@@ -1,6 +1,7 @@
 #include "HelloWorldScene.h"
 #include "SimpleAudioEngine.h"
 
+
 USING_NS_CC;
 
 Scene* HelloWorld::createScene()
@@ -27,54 +28,24 @@ bool HelloWorld::init()
     {
         return false;
     }
+
+	auto visibleSize = Director::getInstance()->getVisibleSize();
+	Vec2 origin = Director::getInstance()->getVisibleOrigin();
+
     
-    auto visibleSize = Director::getInstance()->getVisibleSize();
-    Vec2 origin = Director::getInstance()->getVisibleOrigin();
+	/*std::string file = "untitled.tmx";
+	auto str = String::createWithContentsOfFile(FileUtils::getInstance()->fullPathForFilename(file.c_str()).c_str());
+	_tileMap = TMXTiledMap::createWithXML(str->getCString(), "");
+	_background = _tileMap->layerNamed("Background");
 
-    /////////////////////////////
-    // 2. add a menu item with "X" image, which is clicked to quit the program
-    //    you may modify it.
+	addChild(_tileMap, -1); */
+	_tileMap = TMXTiledMap::create("untitled.tmx");
+	_background = _tileMap->layerNamed("base");
 
-    // add a "close" icon to exit the progress. it's an autorelease object
-    auto closeItem = MenuItemImage::create(
-                                           "CloseNormal.png",
-                                           "CloseSelected.png",
-                                           CC_CALLBACK_1(HelloWorld::menuCloseCallback, this));
-    
-    closeItem->setPosition(Vec2(origin.x + visibleSize.width - closeItem->getContentSize().width/2 ,
-                                origin.y + closeItem->getContentSize().height/2));
-
-    // create menu, it's an autorelease object
-    auto menu = Menu::create(closeItem, NULL);
-    menu->setPosition(Vec2::ZERO);
-    this->addChild(menu, 1);
-
-    /////////////////////////////
-    // 3. add your codes below...
-
-    // add a label shows "Hello World"
-    // create and initialize a label
-    
-    auto label = Label::createWithTTF("Hello World", "fonts/Marker Felt.ttf", 24);
-    
-    // position the label on the center of the screen
-    label->setPosition(Vec2(origin.x + visibleSize.width/2,
-                            origin.y + visibleSize.height - label->getContentSize().height));
-
-    // add the label as a child to this layer
-    this->addChild(label, 1);
-
-    // add "HelloWorld" splash screen"
-    auto sprite = Sprite::create("HelloWorld.png");
-
-    // position the sprite on the center of the screen
-    sprite->setPosition(Vec2(visibleSize.width/2 + origin.x, visibleSize.height/2 + origin.y));
-
-    // add the sprite as a child to this layer
-    this->addChild(sprite, 0);
-    
-    return true;
+	this->addChild(_tileMap);
+	return true;
 }
+
 
 
 void HelloWorld::menuCloseCallback(Ref* pSender)
@@ -85,3 +56,24 @@ void HelloWorld::menuCloseCallback(Ref* pSender)
     exit(0);
 #endif
 }
+/*bool HelloWorld::init()
+{
+    if (!Layer::init())
+	   {
+		        return false;
+		}
+	
+		
+		Size visibleSize = Director::getInstance()->getVisibleSize();
+		Point origin = Director::getInstance()->getVisibleOrigin();
+	
+	
+		_tileMap = TMXTiledMap::create("untitled/MiddleMap.tmx");                            
+		addChild(_tileMap, 0, 100);                                                
+	
+	
+	
+		
+    return true;
+}*/
+
--- Tatica/Classes/HelloWorldScene.h
+++ Tatica/Classes/HelloWorldScene.h
@@ -2,9 +2,14 @@
 #define __HELLOWORLD_SCENE_H__
 
 #include "cocos2d.h"
+USING_NS_CC;
 
 class HelloWorld : public cocos2d::Layer
 {
+private:
+	CCTMXTiledMap *_tileMap;
+	CCTMXLayer *_background; 
+
 public:
     static cocos2d::Scene* createScene();
 
@@ -15,6 +20,29 @@ public:
     
     // implement the "static create()" method manually
     CREATE_FUNC(HelloWorld);
+
 };
 
+
+
 #endif // __HELLOWORLD_SCENE_H__
+
+/*#ifndef __HELLOWORLD_SCENE_H__
+#define __HELLOWORLD_SCENE_H__
+
+#include "cocos2d.h"
+
+class HelloWorld : public cocos2d::Layer
+{
+    cocos2d::TMXTiledMap* _tileMap;                                     
+    cocos2d::Sprite *_player;                                          
+public:
+	static cocos2d::Scene* createScene();
+    virtual bool init();
+
+	  CREATE_FUNC(HelloWorld);
+};
+
+
+#endif // __HELLOWORLD_SCENE_H__  */
+
